9c2a91e2977be2fd9aafb44397c9977ff3d576a4
Ayub <auyub.arbiev@gmail.com>
2022-12-22 19:50:13 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,30 @@
+﻿int N = new Random().Next(1, 101);
+Console.WriteLine($"N = {N}");
+
+int heats = 0;      //0
+int tails = 0;      //1
+int[] coins = new int[N];
+
+foreach (int item in coins)
+{
+    int side = new Random().Next(0, 2);
+    coins[item] = side;
+  Console.Write($"[{coins[item]}] ");
+    if (coins[item] == 0)
+    {
+        heats++;
+    }
+    else
+    {
+        tails++;
+    }
+}
+Console.WriteLine($"\nheats - {heats}\ntails - {tails}");
+if (heats > tails)
+{
+    Console.WriteLine($"{tails} minimum number of coins to flip");
+}
+else
+{
+    Console.WriteLine($"{heats} minimum number of coins to flip");
+}
